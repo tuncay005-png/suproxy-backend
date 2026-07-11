0226ebd0827f37f2cb60b1eadfd7df7ec5c05acd
SuProxy Team <suproxy@example.com>
2026-07-12 00:31:42 +0300

    fix: add nodeID parameter in client_repository_test (List table-driven tests)

--- test/integration/client_repository_test.go
+++ test/integration/client_repository_test.go
@@ -550,7 +550,18 @@ func TestClientRepository_List(t *testing.T) {
 			err = userRepo.Create(ctx, user)
 			require.NoError(t, err)
 
-			instance, err := testutil.CreateTestXrayInstanceWithDefaults()
+			// Create dependencies - server and node first
+			testServer, err := testutil.CreateTestServerWithDefaults()
+			require.NoError(t, err)
+			err = app.Container.ServerRepository.Create(ctx, testServer)
+			require.NoError(t, err)
+
+			testNode, err := testutil.CreateTestNodeWithDefaults(testServer.ID)
+			require.NoError(t, err)
+			err = app.Container.NodeRepository.Create(ctx, testNode)
+			require.NoError(t, err)
+
+			instance, err := testutil.CreateTestXrayInstanceWithDefaults(testNode.ID)
 			require.NoError(t, err)
 			err = instanceRepo.Create(ctx, instance)
 			require.NoError(t, err)
